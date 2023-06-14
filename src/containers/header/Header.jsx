2e6d4f1663d096275c8e17a9b666633a9cb2d14c
arjunKa <32141878+arjunKa@users.noreply.github.com>
2023-06-13 21:06:26 -0400

    Updated header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
       <div className='portfolio__header-content'>
         <h1 className='gradient__text'>Hi, I'm Arjun</h1>
         <div className='portfolio__header-content-details'>
-          <p>I'm a Software Engineering student in my final year. I have experience working as a software developer and engineer,
-            and for my projects some skils I have used are React.js, .NET Core, and Java.</p>
+          <p>I'm a Software Engineering student in my final year. I have experience working as a software developer and engineer; 
+            my projects have used React.js, .NET Core, Java and more.</p>
         </div>
 
       </div>
