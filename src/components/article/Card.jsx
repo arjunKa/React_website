3a4ae97dd1fe83038f2b58ad5df599611336ec7c
arjunKa <32141878+arjunKa@users.noreply.github.com>
2023-12-17 01:57:19 -0500

    Update Card.jsx

--- src/components/article/Card.jsx
+++ src/components/article/Card.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import './card.css'
-import gitLogo from '../../assets/github.svg'
-import GitHubButton from 'react-github-btn'
+import React from "react";
+import "./card.css";
 
-
-const Card = ({linkToProject, skills, text, GithubLink}) => (
-  
+const Card = ({ linkToProject, skills, text }) => (
   <div className="portfolio__blog-container_article">
-
-
     <div className="portfolio__blog-container_article-content">
-      
+      <a href={linkToProject}>
+        <h3>{text}</h3>
+      </a>
 
-    <a href={linkToProject}><h3>{text}</h3></a>
-        
-      <div className='skillsUsed'><h4>Skills Used: </h4> <p>{skills}</p> </div> 
-      
-      <div className="portfolio__blog-container_article-content_view-project"> <a href={linkToProject}> <p>View Project</p> </a> </div>
+      <div className="skillsUsed">
+        <h4>Skills Used: </h4> <p>{skills}</p>{" "}
+      </div>
+
+      <div className="portfolio__blog-container_article-content_view-project">
+        {" "}
+        <a href={linkToProject}>
+          {" "}
+          <p>View Project</p>{" "}
+        </a>{" "}
+      </div>
     </div>
   </div>
-
-
 );
 
-
-export default Card
+export default Card;
